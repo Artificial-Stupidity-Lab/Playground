8f5282d597e7899f4fe14d4b7ce93b38f5dff88d
Mpilo Mbulelo Tatana <81185746+Artificial-Stupidity-Lab@users.noreply.github.com>
2022-10-03 22:59:50 +0200

    create_ml_housingData

---
+++ housingData.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+
